46bedc5d4f3c3fdfe63dfbe5a047575cf0f9324d
Darvell Long <contact@darvell.me>
2013-05-13 01:06:57 +1000

    Initial commit

---
+++ Promote Murder/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  Promote Murder
+//
+
+#import <Foundation/Foundation.h>
+#import "NSString_BibleMurder.h"
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool
+    {
+        NSLog(@"%@",[NSString stringByTakingARandomPassageFromTheBibleThatPromotesMurder]);
+    }
+    return 0;
+}
+
